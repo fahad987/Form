24a8ffe3d2f0cfc9c7ae5cf046b0cf888e45cc89
Fahad <fahad.rehman365@yahoo.com>
2019-06-28 16:58:25 +0500

    Updates

---
+++ precache-manifest.1783649def0bcfb5129c54417c89bbbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d758d6f97229c442bc8736f93572ed2",
+    "url": "/Form/index.html"
+  },
+  {
+    "revision": "52523688d267663b77c4",
+    "url": "/Form/static/css/main.a2fa8a6b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Form/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "52523688d267663b77c4",
+    "url": "/Form/static/js/main.71bd2a5d.chunk.js"
+  },
+  {
+    "revision": "e65650ef5c332fab4c74",
+    "url": "/Form/static/js/runtime~main.100ea07f.js"
+  }
+]);
