b0136ea17935a7c49ab3b2f7cf8129ae5acf9c45
Fahad <fahad.rehman365@yahoo.com>
2019-06-28 16:40:32 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App  extends Component{
+  render(){
+    return(
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <h1>Register</h1>
+          <form onSubmit={this.handleSubmit} noValidate>
+            <div className="firstName">
+              <label htmlFor="firstName">First Name</label>
+         <input 
+            type="text" 
+            className="" 
+            placeholder="First Name" 
+            name="firstname" 
+            noValidate
+            onchange={this.handleChange}
+            />
+            </div>
+    
+            <div className="lastName">
+              <label htmlFor="lastName">Last Name</label>
+         <input 
+            type="text" 
+            className="" 
+            placeholder="Last Name" 
+            name="lastname" 
+            noValidate
+            onchange={this.handleChange}
+            />
+            </div>
 
+            <div className="email">
+              <label htmlFor="email">Email</label>
+         <input 
+            type="email" 
+            className="" 
+            placeholder="email" 
+            name="email" 
+            noValidate
+            onchange={this.handleChange}
+            />
+            </div>
+    
+            <div className="password">
+              <label htmlFor="password">Password</label>
+         <input 
+            type="text" 
+            className="" 
+            placeholder="Password" 
+            name="password" 
+            noValidate
+            onchange={this.handleChange}
+            />
+            </div>
+    
+            <div className="confirmPassword">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+         <input 
+            type="text" 
+            className="" 
+            placeholder="Confirm Password" 
+            name="confirmPassword" 
+            noValidate
+            onchange={this.handleChange}
+            />
+            </div>
+            <div className="createAccount">
+               <button type="submit">Register Now</button>
+
+            </div>
+          </form>
+
+        </div>
+        <small>Already Have an Account? Sign In</small>
+      </div> 
+    );
+  }
+}
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -10,3 +10,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
